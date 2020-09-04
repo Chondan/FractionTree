fc1a092c091090303b71f6329168bf3aeb15edad
Chondan Susuwan <50775454+Chondan@users.noreply.github.com>
2020-09-04 11:51:33 +0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -4,20 +4,17 @@ var x;
 function setup(){
     let c = createCanvas(400,400);
     slider = createSlider(0,TWO_PI,PI/4,0.01);
-
 }
-function draw(){
 
+function draw(){
     background(5);
     angle = slider.value();
     var len = 100;
     stroke(255);
-
     translate(200,height);
     branch(100);
-
-
 }
+
 function branch(len){
   line(0,0,0,-len);
   translate(0,-len);
@@ -38,7 +35,4 @@ function branch(len){
       branch(len*0.67);
       pop();
     }
-
-
-
 }
